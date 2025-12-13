8a73179953a8422e00371e3e8590f543b3da41cd
Syam-2211 <syamsona2009@gmail.com>
2025-12-13 10:28:44 +0530

    Update status.js

--- plugins/status.js
+++ plugins/status.js
@@ -1,32 +1,8 @@
-const send = require("../utils/send");
-const os = require("os");
+import send from "../utils/send.js";
 
-const startTime = Date.now();
-
-module.exports = {
+export default {
   name: "status",
   execute: async (sock, msg) => {
-    const jid = msg.key.remoteJid;
-
-    // Calculate uptime
-    const uptimeMs = Date.now() - startTime;
-    const uptimeSec = Math.floor(uptimeMs / 1000);
-    const uptimeMin = Math.floor(uptimeSec / 60);
-    const uptimeHr = Math.floor(uptimeMin / 60);
-
-    const uptimeStr = `${uptimeHr}h ${uptimeMin % 60}m ${uptimeSec % 60}s`;
-
-    // Count plugins
-    const pluginCount = Object.keys(require.cache)
-      .filter((f) => f.includes("/plugins/"))
-      .length;
-
-    // System info
-    const memoryUsage = (os.totalmem() - os.freemem()) / (1024 * 1024);
-    const cpuModel = os.cpus()[0].model;
-
-    await send(sock, jid, {
-      text: `📊 Bot Status\n\n⏱ Uptime: ${uptimeStr}\n🔌 Plugins Loaded: ${pluginCount}\n💻 CPU: ${cpuModel}\n🧠 Memory Used: ${memoryUsage.toFixed(2)} MB`
-    });
+    await send(sock, msg.key.remoteJid, { text: "📊 Bot is running fine." });
   }
 };
