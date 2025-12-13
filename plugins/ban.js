aff97a08a1f5b1298273a4a8d50141f7ae4b67fa
Syam-2211 <syamsona2009@gmail.com>
2025-12-13 10:29:03 +0530

    Update ban.js

--- plugins/ban.js
+++ plugins/ban.js
@@ -1,18 +1,9 @@
-const send = require("../utils/send");
+import send from "../utils/send.js";
 
-module.exports = {
+export default {
   name: "ban",
-  description: "Ban a user manually",
   execute: async (sock, msg, args) => {
-    const from = msg.key.remoteJid;
-    const mentioned = msg.message.extendedTextMessage?.contextInfo?.mentionedJid || [];
-
-    if (!mentioned.length) {
-      return send(sock, from, { text: "🚫 Mention a user to ban." });
-    }
-
-    const target = mentioned[0];
-    await send(sock, from, { text: `🚫 ${target} has been banned.` });
-    // Add ban logic here (kick from group, etc.)
+    const user = args[0];
+    await send(sock, msg.key.remoteJid, { text: `🚫 User ${user} banned.` });
   }
 };
