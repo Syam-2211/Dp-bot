2dc3b21404186714e316e4350a5e063eaaf3257e
Syam-2211 <syamsona2009@gmail.com>
2025-12-13 10:30:20 +0530

    Update warn.js

--- plugins/warn.js
+++ plugins/warn.js
@@ -1,25 +1,9 @@
-const send = require("../utils/send");
-const warns = {};
+import send from "../utils/send.js";
 
-module.exports = {
+export default {
   name: "warn",
-  description: "Warn a user, auto-ban after 3 warnings",
   execute: async (sock, msg, args) => {
-    const from = msg.key.remoteJid;
-    const mentioned = msg.message.extendedTextMessage?.contextInfo?.mentionedJid || [];
-
-    if (!mentioned.length) {
-      return send(sock, from, { text: "⚠️ Mention a user to warn." });
-    }
-
-    const target = mentioned[0];
-    warns[target] = (warns[target] || 0) + 1;
-
-    if (warns[target] >= 3) {
-      await send(sock, from, { text: `🚫 ${target} has been banned (3 warnings).` });
-      // Add ban logic here (kick from group, etc.)
-    } else {
-      await send(sock, from, { text: `⚠️ ${target} warned (${warns[target]}/3).` });
-    }
+    const user = args[0];
+    await send(sock, msg.key.remoteJid, { text: `⚠️ Warning issued to ${user}.` });
   }
 };
